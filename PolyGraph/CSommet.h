c7d1c99a1855909cef64465981153b7ebfa7bcbe
Mehdi <mehdiaml@icloud.com>
2024-03-13 09:10:41 +0100

    Base commentaire

--- PolyGraph/CSommet.h
+++ PolyGraph/CSommet.h
@@ -1,4 +1,22 @@
 #pragma once
+/******************************************************
+* CLASSE : CSommet
+*******************************************************
+* ROLE : Interface de la classe CSommet
+*******************************************************
+* VERSION : 1.0
+* AUTEUR : Mehdi AMELLADI et Raphaël AVELINE
+* DATE : 13/03/2024
+*******************************************************
+* INCLUSIONS EXTERNES :
+*/
+
+/* TYPES :
+* Pas de type particulier déclaré
+*/
+
+/* VARIABLES:
+*/
 class CSommet
 {
 };
