92dc00cce0279422f5901c1e58ea52d634b01f82
Mehdi <mehdiaml@icloud.com>
2024-03-13 10:07:50 +0100

    Modification interface CSommet

---
+++ PolyGraph/CGraph.h
@@ -0,0 +1,6 @@
+#pragma once
+#include "CGraphOrient.h"
+class CGraph : public CGraphOrient
+{
+};
+
--- PolyGraph/CGraphOrient.h
+++ PolyGraph/CGraphOrient.h
@@ -1,4 +1,25 @@
 #pragma once
+/******************************************************
+* CLASSE : CGraphOrient pour représenter un graphe 
+* orienté
+*******************************************************
+* ROLE : Interface de la classe CGraphOrient permettant
+* de créer des graphes orientés et d'effectuer des
+* opérations sur celui-ci
+*******************************************************
+* VERSION : 1.0
+* AUTEUR : Mehdi AMELLADI et Raphaël AVELINE
+* DATE : 13/03/2024
+*******************************************************
+* INCLUSIONS EXTERNES :
+*/
+
+/* TYPES :
+* Pas de type particulier déclaré
+*/
+
+/* VARIABLES:
+*/
 class CGraphOrient
 {
 };
