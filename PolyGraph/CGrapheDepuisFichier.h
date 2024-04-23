f082b458488560a66b484a181f4fd61ad5654f74
Raphaël Aveline <aveline.raphael@gmail.com>
2024-04-23 18:27:55 +0200

    création de CGrapheDepuisFichier

---
+++ PolyGraph/CGrapheDepuisFichier.h
@@ -0,0 +1,29 @@
+#pragma once
+/******************************************************
+* CLASSE : CGrapheDepuisFichier
+*******************************************************
+* ROLE : Classe permettant de construire des graphes
+* depuis des fichiers
+*******************************************************
+* VERSION : 1.0
+* AUTEUR : Mehdi AMELLADI et Raphael AVELINE
+* DATE : 23/04/2024
+*******************************************************
+* INCLUSIONS EXTERNES :
+*/
+
+#include <iostream>
+
+using namespace std;
+
+/* TYPES :
+* Pas de type particulier declare
+*/
+
+/* VARIABLES:
+*/
+
+class CGrapheDepuisFichier
+{
+};
+
