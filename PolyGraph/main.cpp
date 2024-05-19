b9f6b388db1e512615208484e408a2afdea56c0f
Raphaël Aveline <aveline.raphael@gmail.com>
2024-05-19 17:49:16 +0200

    version finale

--- PolyGraph/main.cpp
+++ PolyGraph/main.cpp
@@ -10,26 +10,16 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-    if (argc != 2)
+    if (argc == 3)
     {
-        cout << "Veuillez rentrez le bon nombre de parametres." << endl;
+        PGraphOrientPondere<CArcPondere, CSommet> GOPGraphe = CCreateurGraph::CCGCreerGrapheOrientPondereFichier(argv[1]);
 
-        PGraphOrientPondere<CArcPondere, CSommet> GPOGraphe = CCreateurGraph::CCGCreerGrapheOrientPondereFichier("./donnees.txt");
+        CAffichage::AFHAfficherGraphe(GOPGraphe);
 
-        CAffichage::AFHAfficherGraphe(GPOGraphe);
-
-        CAffichage::AFHAfficherCycleHamiltonien(GPOGraphe, "O");
-
-        COperationsSurGraphe::OSGInverserArcs(GPOGraphe);
-
-        CAffichage::AFHAfficherGraphe(GPOGraphe);
-
-        CAffichage::AFHAfficherCycleHamiltonien(GPOGraphe, "O");
+        CAffichage::AFHAfficherCycleHamiltonien(GOPGraphe, argv[2]);
     }
-    else
+    else if (argc == 2)
     {
-        //PGraph<CArc, CSommet> GPOGraphe = CCreateurGraph::CCGCreerGrapheFichier(argv[1]);
-
         PGraphOrientPondere<CArcPondere, CSommet> GPOGraphe = CCreateurGraph::CCGCreerGrapheOrientPondereFichier(argv[1]);
 
         CAffichage::AFHAfficherGraphe(GPOGraphe);
@@ -37,8 +27,14 @@ int main(int argc, char* argv[])
         COperationsSurGraphe::OSGInverserArcs(GPOGraphe);
 
         CAffichage::AFHAfficherGraphe(GPOGraphe);
+    }
+    else
+    {
+        cout << "Veuillez rentrez le bon nombre de parametres." << endl;
 
-        cout << COperationsSurGraphe::OSGVerifierGrapheComplet(GPOGraphe) << endl;
+        cout << "Pour creer un graphe pondere et l'inverser, entrer en parametre le chemin d'acces au fichier contenant les infos d'initialisation du graphe;" << endl;
+
+        cout << "Pour creer un graphe pondere et afficher son cycle hamiltonien, entrer en parametre le chemin d'acces au fichier et le sommet source du cycle (qui doit appartenir au graphe)." << endl;
     }
 }
 
